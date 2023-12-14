430809c0bbbb17de9b1e744b9c27dc4ca0129a53
fsensoli <fedesensoli1997@gmail.com>
2023-12-14 23:06:48 +0100

    about text positioning

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,26 +15,29 @@ export default function About() {
                     Sono Laetitia Bof Dietista Nutrizionista<br></br>
                     Ho frequentato il Liceo Scientifico Tradizionale, avendo fin da subito le idee molto chiare su cosa 
                     avrei voluto fare, infatti, dopo aver ricevuto il diploma ho subito iniziato a preparare il Test 
-                    d’Ammissione al corso di Dietistica all’università degli Studi di Milano. Durante questo percorso 
-                    ho vissuto a Milano, il che mi ha permesso di venire in contatto con tante diverse mentalità, modi 
-                    di fare, ideologie e pensieri. Credo sia stato un percorso molto formativo, anche se impegnativo; 
-                    ha richiesto costanza, perseveranza e precisione.<br></br><br></br>
-                    Durante il periodo di formazione ho trascorso un anno al Poliambulatorio Auxologico in diverse 
-                    sedi, qui ho potuto perfezionare la tecnica ambulatoriale relativa al trattamento del sovrappeso e 
-                    dell’obesità da moderata a severa-morbigena (trattando anche la chirurgia bariatrica). Ho, inoltre, 
-                    avuto la fortuna di poter lavorare anche nel Team del MAC (macroattività ambulatoriale 
-                    complessa) un percorso terapeutico-riabilitativo per obesità che comprende un programma di 
-                    trattamento multidisciplinare il quale integra interventi di gruppo e individuali.<br></br>
-                    Ho trascorso anche un anno all’ospedale Policlinico di Milano dove ho avuto la possibilità di 
-                    ampliare le mie conoscenze e abilità nel trattamento di svariate patologie, tra cui: Diabete, 
-                    Dislipidemie (ipercolesterolemia, ipertrigliceridemia, basse HDL), Ipertensione, Malnutrizione 
-                    dovuta a malattie maggiori (malattie , epatopatie, insufficienza renale), disturbi del comportamento 
-                    alimentare.<br></br><br></br>
-                    Da sempre coltivo una grande passione per lo sport e l’ambiente sportivo in generale, per questo 
-                    ho deciso di frequentare un Master di specializzazione nell’ambito di Nutrizione e Integrazione nel 
-                    soggetto sportivo, Master con sede a Torino, riconosciuto dal ENSES (European Sport Nutrition 
-                    Society).
+                    d’Ammissione al corso di Dietistica all’università degli Studi di Milano.
                 </div>
+            </div>
+            <div className='aboutTextOnly'>
+                Durante questo percorso 
+                ho vissuto a Milano, il che mi ha permesso di venire in contatto con tante diverse mentalità, modi 
+                di fare, ideologie e pensieri. Credo sia stato un percorso molto formativo, anche se impegnativo; 
+                ha richiesto costanza, perseveranza e precisione.<br></br><br></br>
+                Durante il periodo di formazione ho trascorso un anno al Poliambulatorio Auxologico in diverse 
+                sedi, qui ho potuto perfezionare la tecnica ambulatoriale relativa al trattamento del sovrappeso e 
+                dell’obesità da moderata a severa-morbigena (trattando anche la chirurgia bariatrica). Ho, inoltre, 
+                avuto la fortuna di poter lavorare anche nel Team del MAC (macroattività ambulatoriale 
+                complessa) un percorso terapeutico-riabilitativo per obesità che comprende un programma di 
+                trattamento multidisciplinare il quale integra interventi di gruppo e individuali.<br></br>
+                Ho trascorso anche un anno all’ospedale Policlinico di Milano dove ho avuto la possibilità di 
+                ampliare le mie conoscenze e abilità nel trattamento di svariate patologie, tra cui: Diabete, 
+                Dislipidemie (ipercolesterolemia, ipertrigliceridemia, basse HDL), Ipertensione, Malnutrizione 
+                dovuta a malattie maggiori (malattie , epatopatie, insufficienza renale), disturbi del comportamento 
+                alimentare.<br></br><br></br>
+                Da sempre coltivo una grande passione per lo sport e l’ambiente sportivo in generale, per questo 
+                ho deciso di frequentare un Master di specializzazione nell’ambito di Nutrizione e Integrazione nel 
+                soggetto sportivo, Master con sede a Torino, riconosciuto dal ENSES (European Sport Nutrition 
+                Society).
             </div>
             <div className='aboutContainer'>
                 <div className='aboutText'>
@@ -42,7 +45,13 @@ export default function About() {
                 messaggio in cui stare “a dieta” non obbliga a rinunce, evitamento o eliminazione ma piuttosto al 
                 raggiungimento di buona consapevolezza che permette di attuare scelte consone per non 
                 rinunciare a nulla ma senza esagerare, non solo ma consente anche di vivere serenamente le 
-                occasioni conviviali imparando l’autogestione.<br></br><br></br>
+                occasioni conviviali imparando l’autogestione.<br></br>
+                </div>
+                <div className='profilePic'>
+                    <img className='profile' src={profilePic1} alt='profile'></img>
+                </div>
+            </div>
+            <div className='aboutTextOnly'>
                 Ciò che è più importante, infatti, è imparare a dosare correttamente gli alimenti nelle porzioni e 
                 frequenze settimanali; se, poi, alla consapevolezza si aggiunge un tocco di curiosità e voglia di 
                 sperimentare si può scoprire che anche “stando a dieta” si possono creare piatti e portate 
@@ -53,10 +62,6 @@ export default function About() {
                 si presente come punto cruciale in diverse occasioni: miglioramento della composizione corporea, 
                 perdita di grasso, aumento delle prestazioni sportive e lavorative, diminuzione dello stress e/o 
                 dolore provocato da patologie croniche..
-                </div>
-                <div className='profilePic'>
-                    <img className='profile' src={profilePic1} alt='profile'></img>
-                </div>
             </div>
             <Footer></Footer>
         </>
